30ecc4237e55cff3269ba4a07ccaf313d8c0f05c
Sureshbabu Mathan <Sureshbabu_Mathan@ad.infosys.com>
2020-09-28 22:00:29 +0530

    Signed-off-by: Sureshbabu Mathan <Sureshbabu_Mathan@ad.infosys.com>

--- src/main/java/com/moviesurl/springboot/controller/MoviesController.java
+++ src/main/java/com/moviesurl/springboot/controller/MoviesController.java
@@ -2,6 +2,8 @@ package com.moviesurl.springboot.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +19,14 @@ public class MoviesController {
 	private serviceInterface slayer;
 
 	@GetMapping("/GetMovie")
-	public List<MovieObject> GetMovie(MovieObject movie) {
-		List<MovieObject> mlist = slayer.GetmovieObj(movie);
-//		if (mlist == null || mlist.isEmpty() == true)
-//			throw new MovieListException("About 0 search records");
-		return mlist;
+	public ResponseEntity<List<MovieObject>> GetMovie(MovieObject movie) {
+		try {
+			if (movie.getCrew() == null && movie.getName() == null && movie.getRank() == null && movie.getRating() == null && movie.getYear() == null)
+				throw new MovieListException("Bad Request");
+			List<MovieObject> mlist = slayer.GetmovieObj(movie);
+			return new ResponseEntity<List<MovieObject>>(mlist, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<List<MovieObject>>(HttpStatus.BAD_REQUEST);
+		}
 	}
 }
